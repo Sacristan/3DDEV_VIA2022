eba4f4431f3c7d2939e0ee248f22d5a662b90f13
Ģirts Ķesteris <girts.kesteris@gmail.com>
2022-04-22 14:24:17 +0300

    hero death

---
+++ Assets/Hero.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StarterAssets;
+
+public class Hero : MonoBehaviour
+{
+    float health = 100;
+    bool isDead = false;
+
+    ThirdPersonController _thirdPersonController;
+
+    private void Start()
+    {
+        _thirdPersonController = GetComponent<ThirdPersonController>();
+    }
+
+    void AddDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0) Die();
+    }
+
+    void Die()
+    {
+        if (isDead) return;
+        isDead = true;
+
+        _thirdPersonController.enabled = false;
+    }
+
+}
